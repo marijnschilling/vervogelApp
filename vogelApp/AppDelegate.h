a76c312391781d5713234a8c112c6554ff014b26
Marijn Schilling <marijn@fonk-amsterdam.com>
2014-11-17 20:54:49 +0100

    new project with storyboard this time

---
+++ vogelApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vogelApp
+//
+//  Created by Marijn Schilling on 17/11/14.
+//  Copyright (c) 2014 marijn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ vogelApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  vogelApp
+//
+//  Created by Marijn Schilling on 17/11/14.
+//  Copyright (c) 2014 marijn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
