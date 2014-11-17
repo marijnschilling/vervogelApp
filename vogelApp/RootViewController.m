b6b7393cd1dcb9846f435a9c7736c83d021879d9
Marijn Schilling <marijn@fonk-amsterdam.com>
2014-11-17 21:53:11 +0100

    record and stop recording the video

--- vogelApp/RootViewController.m
+++ vogelApp/RootViewController.m
@@ -8,9 +8,13 @@
 
 #import "RootViewController.h"
 
-@interface RootViewController ()
+@interface RootViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property(nonatomic, strong) UIImagePickerController * imagePicker;
+@property(nonatomic, strong) UILabel *stopRecordLabel;
+@property(nonatomic, strong) UILabel *recordLabel;
+@property(nonatomic, strong) id videoURL;
+@property(nonatomic, strong) MPMoviePlayerController *player;
 @end
 
 @implementation RootViewController
@@ -29,6 +33,28 @@
     UIImage *image = [UIImage imageNamed:@"cameraOverlay"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
     [cameraOverlayView addSubview:imageView];
+
+    self.recordLabel = [[UILabel alloc] init];
+    self.recordLabel.text = @"Record >>>";
+    [self.recordLabel sizeToFit];
+    self.recordLabel.frame = CGRectMake(self.view.bounds.size.width/2-self.recordLabel.frame.size.width/2, self.view.bounds.size.height/2-self.recordLabel.frame.size.height/2,self.recordLabel.frame.size.width,self.recordLabel.frame.size.height);
+
+    [self.recordLabel setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *recordGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapRecord:)];
+    [self.recordLabel addGestureRecognizer:recordGesture];
+    [cameraOverlayView addSubview:self.recordLabel];
+    
+    self.stopRecordLabel = [[UILabel alloc] init];
+    self.stopRecordLabel.text = @"Stop";
+    self.stopRecordLabel.textColor = [UIColor redColor];
+    [self.stopRecordLabel sizeToFit];
+    self.stopRecordLabel.frame = CGRectMake(self.view.bounds.size.width/2-self.stopRecordLabel.frame.size.width/2, self.view.bounds.size.height/2-self.stopRecordLabel.frame.size.height/2,self.stopRecordLabel.frame.size.width,self.stopRecordLabel.frame.size.height);
+    [self.stopRecordLabel setHidden:YES];
+
+    [self.stopRecordLabel setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *stopRecordGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapStopRecord:)];
+    [self.stopRecordLabel addGestureRecognizer:stopRecordGesture];
+    [cameraOverlayView addSubview:self.stopRecordLabel];
 
     [self presentViewController:self.imagePicker animated:animated completion:nil];
     self.imagePicker.cameraOverlayView = cameraOverlayView;
@@ -55,5 +81,50 @@
     self.imagePicker.videoQuality = UIImagePickerControllerQualityType640x480;
     self.imagePicker.delegate = self;
 }
+
+- (void)didTapRecord:(id)didTapRecord {
+
+    [self.recordLabel setHidden:YES];
+    [self.stopRecordLabel setHidden:NO];
+    [self.imagePicker startVideoCapture];
+}
+
+- (void)didTapStopRecord:(id)didTapStopRecord {
+
+    [self.stopRecordLabel setHidden:YES];
+    [self.imagePicker stopVideoCapture];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+
+    self.videoURL = info[UIImagePickerControllerMediaURL];
+    [self Play];
+
+}
+
+-(void)Play
+{
+
+    NSData *videoData = [NSData dataWithContentsOfURL:self.videoURL];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *tempPath = [documentsDirectory stringByAppendingFormat:@"/vid1.mp4"];
+
+    BOOL success = [videoData writeToFile:tempPath atomically:NO];
+
+    self.player = [[MPMoviePlayerController alloc]initWithContentURL:self.videoURL];
+
+    self.player.shouldAutoplay = NO;
+    self.player.view.frame = self.view.bounds;
+
+    [self.view addSubview:self.player.view];
+
+    self.player.scalingMode = MPMovieScalingModeAspectFit;
+
+    self.player.fullscreen = YES;
+
+    [self.player play];
+}
+
 
 @end
