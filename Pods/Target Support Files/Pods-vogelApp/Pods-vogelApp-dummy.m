ac2c3462875c000978f7def143c8891b79ae4571
Marijn Schilling <marijn@fonk-amsterdam.com>
2014-11-18 20:35:11 +0100

    Add podfile and manual layout

---
+++ Pods/Target Support Files/Pods-vogelApp/Pods-vogelApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vogelApp : NSObject
+@end
+@implementation PodsDummy_Pods_vogelApp
+@end
---
+++ Pods/Target Support Files/Pods-vogelApp/Pods-vogelApp-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// EEEManualLayout
+#define COCOAPODS_POD_AVAILABLE_EEEManualLayout
+#define COCOAPODS_VERSION_MAJOR_EEEManualLayout 0
+#define COCOAPODS_VERSION_MINOR_EEEManualLayout 1
+#define COCOAPODS_VERSION_PATCH_EEEManualLayout 2
+
